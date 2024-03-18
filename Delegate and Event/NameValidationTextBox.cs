470108a14a032dde2769354286e8f4a32e464bd2
Zeevte <s0533146568@gmail.com>
2024-03-18 16:56:30 +0200

    Add project files.

---
+++ Delegate and Event/NameValidationTextBox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Delegate_and_Event
+{
+    internal class NameValidationTextBox: TextBox
+    {
+        
+
+    }
+}
