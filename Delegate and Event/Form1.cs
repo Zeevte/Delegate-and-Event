ec56e7135da790bf7a4e82869c7489f701c7e193
Zeevte <s0533146568@gmail.com>
2024-03-18 17:53:15 +0200

    שלבים 6-8

--- Delegate and Event/Form1.cs
+++ Delegate and Event/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 
 namespace Delegate_and_Event
 {
+    //הצערה על דליגהט
     public delegate int Calc(int x, int y);
     public partial class Form1 : Form
     {
@@ -20,13 +21,17 @@ namespace Delegate_and_Event
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //שימוש בפונקציה
             Fun1(Add);
         }
+
+        //פונקציה שעונה על ההגדרה של דליגהט
         public int Add(int x, int y)
         {
             return x + y;
         }
 
+        //פונקציה שמפנה לדליגהט
         public void Fun1(Calc calc)
         {
             calc(5, 4);
--- Delegate and Event/NameValidationTextBox.cs
+++ Delegate and Event/NameValidationTextBox.cs
@@ -10,11 +10,19 @@ namespace Delegate_and_Event
 {
     public class NameValidationTextBox: TextBox
     {
+        public EventHandler InvalidKeyPressed;
         // Constructor
         public NameValidationTextBox()
         {
+            KeyPress += ValidationTextBox_KeyPress;
 
         }
+        private void ValidationTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+
 
     }
 }
